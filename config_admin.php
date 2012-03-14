8f878702ac3d75858f08761dcbb8a8dbcb318ae5
beshkin <beshkin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-14 19:59:05 +0000

    tagging version 0.5.1
    git-svn-id: https://plugins.svn.wordpress.org/e-commerce-multi-currency-support/tags/0.5.1@519052 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config_admin.php
@@ -0,0 +1,48 @@
+<?
+/**
+ * Created by JetBrains WebStorm.
+ * User: misha
+ * Date: 3/14/12
+ * To change this template use File | Settings | File Templates.
+ */
+
+ $data = get_option('ecom_currency_convert');
+ if (isset($_POST['currency_source']))
+ {
+    foreach ($_POST as $opt=>$val){
+        if (preg_match('/currency_.*/',$opt)) $data[$opt] = attribute_escape($val);
+    }
+    update_option('ecom_currency_convert', $data);
+ }
+?>
+ <div class="wrap">
+     <div id="icon-options-general" class="icon32">
+         <br/>
+     </div>
+     <div style="width:400px;float:left;">
+     <form method="post" >
+     <h2>Zmanim widget</h2>
+
+ 	<!--<h3>Basic options</h3>-->
+ 	<?include "config.php";?>
+         <h3>Advanced options</h3>
+         <label for="currency_source">Select site to get information:</label>
+                         <select name="currency_source">
+                            <?
+                                $sources = array("wpsc"=>"Build-in WPSC", "google"=>"Google");
+                                foreach ($sources as $ind=>$val)
+                                {
+                                    print '<option value="'.$ind.'"';
+                                    if ($data['currency_source']==$ind) print ' selected=selected';
+                                    print '>'.$val.'</option>';
+                                }
+                            ?>
+                         </select>
+        <p class="submit">
+                <input type="submit" name="Submit" class="button-primary" value="Save Changes" id="submitCalendarAdd"/>
+                </p>
+         </form>
+     </div>
+ </div>
+
+
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Misha Beshkin and WPSC team - Dan Milward, Tom Howard, Jeffry Ghaz
 Tags: e-commerce, shop, cart, featured product,featured,sticky product, ecommerce, currency
 Requires at least: 2.9
 Tested up to: 3.1.1
-Stable tag: 0.4.5
+Stable tag: 0.5.1
 
 
 == Description ==
@@ -16,6 +16,9 @@ e-Commerce Multi Currency support is a Add-on Currency Widget that allows your c
 2. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Changelog ==
+= 0.5 =
+- added advanced options page
+- added possibility to choose Google convertion mechanism.
 = 0.4.5 =
 - now currency is changed in shopping cart as well.
 = 0.4.4 =
